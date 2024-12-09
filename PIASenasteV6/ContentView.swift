83c00eb05f524d13d7b3c4fcb7ff6c7c60709e20
koddaz <Albert.niklasson@gmail.com>
2024-12-09 09:10:29 +0100

    Initial Commit

---
+++ PIASenasteV6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PIASenasteV6
+//
+//  Created by Albert Niklasson on 2024-12-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PIASenasteV6/PIASenasteV6App.swift
@@ -0,0 +1,17 @@
+//
+//  PIASenasteV6App.swift
+//  PIASenasteV6
+//
+//  Created by Albert Niklasson on 2024-12-09.
+//
+
+import SwiftUI
+
+@main
+struct PIASenasteV6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
